6e3683c83b436257330098bd6bf62655a677eed8
Annbpiu <Annbpiu@gmail.com>
2025-02-13 10:07:02 +0200

    first upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+export const Header = () => {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <div className="flex">
+                        <img src="./vite.svg" alt="#" />
+                        <span>aplot.ua</span>
+                    </div>
+                    <div>
+                        <span>Чернівецька обл., с.Великий Кучурів,</span>
+                        <span>вул.Головна 31-О</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>+38 (050) 375 55 31</span>
+                        <span>пн-пт 09:00 - 17:00</span>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
