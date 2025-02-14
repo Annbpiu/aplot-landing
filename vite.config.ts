4afd5c13eb118bf9bb8bc267745a5869dcdf5bf9
Annbpiu <Annbpiu@gmail.com>
2025-02-14 11:13:53 +0200

    fix gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/index.css";`
+        additionalData: `@import "./index.css";`
       },
     },
   },
