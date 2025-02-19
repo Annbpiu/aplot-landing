95b7482375e9380087b43a792de9d3bf81640e06
Annbpiu <Annbpiu@gmail.com>
2025-02-19 15:36:04 +0200

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Player from "./Player";
 
 export const Header = () => {
     return (
-        <span className="relative w-full">
+        <div className="relative min-h-screen w-full">
             <div className="relative w-full h-screen">
                 <div className="absolute inset-0">
                     <Player/>
@@ -13,6 +13,6 @@ export const Header = () => {
                     <NavMenu/>
                 </div>
             </div>
-        </span>
+        </div>
     );
 };
