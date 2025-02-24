8ecf52c9fc30e685296001f6c37e0420eadceb8a
Annbpiu <Annbpiu@gmail.com>
2025-02-24 11:22:28 +0200

    try to fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Player from "./Player";
 
 export const Header = () => {
     return (
-        <div className="relative md:min-h-screen w-full">
+        <div className="relative md:h-svh w-full">
             <div className="relative w-full min-h-screen">
                 <div className="absolute inset-0">
                     <Player/>
